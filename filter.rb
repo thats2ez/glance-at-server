684a22eda442a039c23e5589c6a10dc2481d5926
Kaiqi <thats2ez@gmail.com>
2014-11-06 20:55:47 -0800

    get requests from a message

--- filter.rb
+++ filter.rb
@@ -1,14 +1,12 @@
-module GlanceAt
-  # request filter
-  class RequestFilter
-    def initialize()
-      @name = 'request'
-    end
 
-    def apply_to(conversation, user)
-      p 'undefined'
-    end
+# request filter
+class RequestFilter
+  def initialize()
+    @name = 'request'
+  end
 
+  def apply_to(conversation, user)
+    p 'undefined'
   end
 
 end
--- nlp.rb
+++ nlp.rb
@@ -1,5 +1,35 @@
-module GlanceAt
-  class NaturalLanguageParser
+require 'treat'
+include Treat::Core::DSL
 
+class NaturalLanguageParser
+
+  @@request_keywords =
+    ['let me know', 'Let me know', 'send me', 'Send me']
+
+  def initialize(message)
+    @name = 'nlp'
+    @message = message
+  end
+
+  def requests
+    self.class.sentences(@message).select { |s| self.class.request? s }
+  end
+
+  private
+
+  def self.sentences(text)
+    paragraph(text).segment(:srx).to_a
+  end
+
+  def self.words(text)
+    sentence(text).tokenize(:ptb).to_a
+  end
+
+  def self.request?(text)
+    if text.end_with? '?'
+      true
+    else
+      @@request_keywords.any? { |w| text.include? w }
+    end
   end
 end
