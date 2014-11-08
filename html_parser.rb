0f9e437f51923e3dc6c6713a2f6ed4945063c128
Kaiqi <thats2ez@gmail.com>
2014-11-08 12:35:40 -0800

    [feature] remove quoted thread

--- html_parser.rb
+++ html_parser.rb
@@ -1,15 +1,23 @@
 require 'nokogiri'
 
-
 class HTMLParser
 
   def initialize(body)
     @doc = Nokogiri::HTML(body)
-    p @doc
+    # p @doc
   end
 
   def remove_quote
+    # delete blockquote nodes
+    @doc.css('blockquote').remove
 
+    # delete node with certain classes
+    @doc.css('.gmail_quote').remove
+    @doc.css('.gmail_extra').remove
+    @doc.css('.moz-signature').remove
+
+    # return the remaining html
+    @doc.inner_html
   end
 
 end
